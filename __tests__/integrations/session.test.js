48e52ebf6418140ad2ad9f3004ee7ed5b190381d
Diogo Basso <diogojbasso@gmail.com>
2021-01-06 15:06:48 -0300

    add squelite and tests

---
+++ __tests__/integrations/session.test.js
@@ -0,0 +1,11 @@
+describe('auth', () => {
+  it('should return a JWT when its called', () => { //test, KISS
+
+  })
+  it('should sum two numbers',()=>{
+    const x = 2
+    const y = 3
+    const sum = x+y
+    expect(sum).toBe(5)
+  })
+})
