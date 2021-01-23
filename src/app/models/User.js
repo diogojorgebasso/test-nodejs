9ccf4f07e89a78a9b3c4abb9788f48b1460da902
Diogo Basso <diogojbasso@gmail.com>
2021-01-23 17:34:20 -0300

    created model generate token w/ JWT

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -22,5 +22,8 @@ module.exports = (sequelize, DataTypes) => {
     //ter acesso ao "this"
     return bcrypt.compare(password, this.passwordhash);
   };
+  User.prototype.generateToken = function () {
+    return jwt.sign({ id: this.id }, process.env.APP_SECRET); //secret deve ser único
+  };
   return User;
 };
