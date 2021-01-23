e06aa885f69abf3f521f613e305284e83b792cd5
Diogo Basso <diogojbasso@gmail.com>
2021-01-23 17:44:26 -0300

    add factory to test

--- __tests__/integrations/session.test.js
+++ __tests__/integrations/session.test.js
@@ -1,41 +1,33 @@
 const request = require("supertest");
-const { User } = require("../../src/models");
+const factory = require("../factories");
 const truncate = require("../utils/truncate");
+
 describe("auth", () => {
   beforeEach(async () => await truncate());
+
   it("should auth with valid credential", async () => {
-    const user = User.create({
-      name: "Diogo",
-      email: "diogo@ensinu.com.br",
-      password: "123123",
-    });
+    const user = await factory.create("User");
     const response = await request(app)
       .post("/sessions")
       .send({ email: user.email, password: "123123" });
 
     expect(response.status).toBe(200);
   });
+
   it("Should not auth with invalid credentials", async () => {
-    const user = User.create({
-      name: "Diogo",
-      email: "diogo@ensinu.com.br",
-      password: "123123",
-    });
+    const user = await factory.create("User");
     const response = await request(app)
       .post("/sessions")
       .send({ email: user.email, password: "123123" });
 
     expect(response.status).toBe(401);
   });
+
   it("Should receive JWT token when auth", async () => {
-    const user = User.create({
-      name: "Diogo",
-      email: "diogo@ensinu.com.br",
-      password: "123123",
-    });
+    const user = await factory.create("User");
     const response = await request(app)
       .post("/sessions")
       .send({ email: user.email, password: "123123" });
+    expect(response.body).toHaveProperty("token");
   });
-  expect(response.body).toHaveProperty("token");
 });
